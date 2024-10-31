c6463165ab62dcae639f8ec99347e3233f5891ac
Annd <ssandreia30@gmail.com>
2024-10-31 14:02:10 -0300

    add a 'removed item' message when deleting items on the list

--- scripts.js
+++ scripts.js
@@ -4,35 +4,72 @@ const form = document.querySelector("form");
 
 form.onsubmit = (event) => {
   event.preventDefault();
+    deleteRemovedMsg();
 
-  const itemContainer = document.createElement("div");
-  itemContainer.classList.add("item-container");
+  if (newItem.value === "") {
+    alert("Adicione um item!");
+  } else {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
 
-  const itemWrapper = document.createElement("div");
-  itemWrapper.classList.add("item-wrapper");
-  
+    const itemWrapper = document.createElement("div");
+    itemWrapper.classList.add("item-wrapper");
 
-  const item = document.createElement("input");
-  item.setAttribute("type", "checkbox");
+    const item = document.createElement("input");
+    item.setAttribute("type", "checkbox");
 
-  const itemLabel = document.createElement("label");
-  itemLabel.textContent = newItem.value;
+    const itemLabel = document.createElement("label");
+    itemLabel.textContent = newItem.value;
 
-  const deleteBtn = document.createElement("img");
-  deleteBtn.setAttribute("src", "./assets/icons/delete.svg");
-  deleteBtn.classList.add("delete-icon");
+    const deleteBtn = document.createElement("img");
+    deleteBtn.setAttribute("src", "./assets/icons/trash-can.svg");
+    deleteBtn.classList.add("delete-icon");
 
-  itemsFieldset.append(itemContainer);
-  itemWrapper.append(item, itemLabel);
-  itemContainer.append(itemWrapper, deleteBtn);
-  newItem.value = "";
+    itemsFieldset.append(itemContainer);
+    itemWrapper.append(item, itemLabel);
+    itemContainer.append(itemWrapper, deleteBtn);
+    newItem.value = "";
 
-  deleteItem(deleteBtn, itemContainer)
-}
+    deleteItem(deleteBtn, itemContainer, itemsFieldset);
+    
+  }
+};
 
-
-function deleteItem(deleteBtn, itemContainer) {
+function deleteItem(deleteBtn, itemContainer, itemsFieldset) {
   deleteBtn.addEventListener("click", () => {
     itemContainer.remove();
-  })
+
+    deleteRemovedMsg();
+
+
+    let removedMsgWrapper = document.createElement("div");
+
+    let warningIcon = document.createElement("img");
+    warningIcon.setAttribute("src", "./assets/icons/warning.svg");
+
+    let deleteIcon = document.createElement("img");
+    deleteIcon.setAttribute("src", "./assets/icons/delete.svg");
+    deleteIcon.classList.add("delete");
+
+    let deleteMsg = document.createElement("span");
+    deleteMsg.textContent = "O item foi removido da lista";
+    removedMsgWrapper.classList.add("removed-msg");
+
+    removedMsgWrapper.append(warningIcon, deleteMsg, deleteIcon);
+    itemsFieldset.append(removedMsgWrapper);  
+
+    deleteIcon.addEventListener("click", () => {
+      removedMsgWrapper.remove();
+    });
+
+  });
 }
+
+function deleteRemovedMsg() {
+   let removedMsg = document.querySelector(".removed-msg");
+
+   if (itemsFieldset.contains(removedMsg)) {
+     removedMsg.remove();
+   } 
+}
+
